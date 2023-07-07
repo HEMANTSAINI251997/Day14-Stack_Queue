c3ba937ac0f4d3379acc4add60f05c87af056509
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-07 14:34:13 +0530

    [HEMANT]Add: ADD Data in Stack

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack_LinkedList stack_LinkedList = new Stack_LinkedList();
+            stack_LinkedList.Add(70);
+            stack_LinkedList.Add(30);
+            stack_LinkedList.Add(56);
+            stack_LinkedList.Display();
+        }
+    }
+}
---
+++ Stack_Queue/Stack_LinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    public class Stack_LinkedList
+    {
+        public Node top;
+        
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.next=null;
+            }
+            else
+            {
+                node.next = top;    
+            }
+            top= node;
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine("Elements = " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
