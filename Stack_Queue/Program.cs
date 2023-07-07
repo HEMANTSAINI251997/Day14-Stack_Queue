34391be255a5f14aa9faf8bc515a3cbccbe43c23
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-07 15:22:14 +0530

    [HEMANT]Add: ADD DATA IN QUEUE

--- Stack_Queue/Program.cs
+++ Stack_Queue/Program.cs
@@ -11,16 +11,43 @@ namespace Stack_Queue
         static void Main(string[] args)
         {
             Stack_LinkedList stack_LinkedList = new Stack_LinkedList();
-            stack_LinkedList.Add(70);
-            stack_LinkedList.Add(30);
-            stack_LinkedList.Add(56);
-            stack_LinkedList.Display();
-            Console.WriteLine("-----------------");
-            stack_LinkedList.Peek();
-            stack_LinkedList.Display();
-            Console.WriteLine("-----------------");
-            stack_LinkedList.Pop();
-            stack_LinkedList.Display();
+            Queue_LinkedList queue_LinkedList = new Queue_LinkedList();           
+            while (true)
+            {
+                Console.WriteLine("WELCOME");
+                Console.WriteLine("Press 1 for Stack ");
+                Console.WriteLine("Press 2 for Queue");
+                Console.WriteLine("Enter the Choice");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        
+                        stack_LinkedList.Add(70);
+                        stack_LinkedList.Add(30);
+                        stack_LinkedList.Add(56);
+                        stack_LinkedList.Display();
+                        Console.WriteLine("-----------------");
+                        stack_LinkedList.Peek();
+                        stack_LinkedList.Display();
+                        Console.WriteLine("-----------------");
+                        stack_LinkedList.Pop();
+                        stack_LinkedList.Display();
+                        break;
+                    case 2:
+                       
+                        queue_LinkedList.Enqueue(70);
+                        queue_LinkedList.Enqueue(30);
+                        queue_LinkedList.Enqueue(56);
+                        queue_LinkedList.Display();
+                        Console.WriteLine("-----------------");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+               
+            }
 
         }
     }
---
+++ Stack_Queue/Queue_LinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    public class Queue_LinkedList
+    {
+        Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("QUEUE IS EMPTY");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.WriteLine("ELEMENT = " + temp.data);
+                temp = temp.next;
+            }
+
+                
+        }
+
+    }
+}
